958cf26e34c3c395e84c2d777dcad1eb3baaa336
Austin Aryain <austinaryain@gmail.com>
2017-03-17 00:54:29 -0500

    Image load for detail view

--- app/src/main/java/com/example/aryai/topmovies/DetailActivity.java
+++ app/src/main/java/com/example/aryai/topmovies/DetailActivity.java
@@ -1,12 +1,19 @@
 package com.example.aryai.topmovies;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -36,6 +43,43 @@ public class DetailActivity extends AppCompatActivity {
         ratingTV.setText("Rating: " + Double.parseDouble(i.getStringExtra("RATING")) + " / 10");
         releaseTV.setText("Release Date: " + i.getStringExtra("RELEASEDATE"));
         languageTV.setText("Language: " + i.getStringExtra("LANGUAGE"));
+
+        new ImageLoadTask(i.getStringExtra("THUMBNAIL"), thumbnailIV).execute();
+
+    }
+
+    public class ImageLoadTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private String url;
+        private ImageView imageView;
+
+        public ImageLoadTask(String url, ImageView imageView) {
+            this.url = url;
+            this.imageView = imageView;
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            try {
+                URL urlConnection = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) urlConnection
+                        .openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            super.onPostExecute(result);
+            imageView.setImageBitmap(result);
+        }
 
     }
 }
